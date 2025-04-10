40948e7b867229963d73acc0256ec56208b5b331
FacuSanca <fsanca@mail.austral.edu.ar>
2025-04-10 17:07:12 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,7 +1,7 @@
 def check_vowels():
     nombre = input()
-    print("Contiene a:" + str("a" in nombre or "A" in nombre))
-    print("Contiene e:" + str("e" in nombre or "E" in nombre))
-    print("Contiene i:" + str("i" in nombre or "I" in nombre))
-    print("Contiene o:" + str("o" in nombre or "O" in nombre))
-    print("Contiene u:" + str("u" in nombre or "U" in nombre))
+    print("Contiene a: " + str("a" in nombre or "A" in nombre))
+    print("Contiene e: " + str("e" in nombre or "E" in nombre))
+    print("Contiene i: " + str("i" in nombre or "I" in nombre))
+    print("Contiene o: " + str("o" in nombre or "O" in nombre))
+    print("Contiene u: " + str("u" in nombre or "U" in nombre))
